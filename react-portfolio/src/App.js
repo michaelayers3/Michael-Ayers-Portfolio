8e9fafabb8c80cc48d55c60e33165014eed2ab85
michaelayers3 <michaelgregoryayers@gmail.com>
2023-08-10 21:56:08 -0500

    added newest project

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -14,6 +14,7 @@ import WTFimage from './images/WTF.PNG';
 import WorkDay from './images/Work-Day-Scheduler.png';
 import pwImage from './images/Password-Generator.png';
 import weatherImage from './images/Weather-Dashboard.png';
+import designerAI from './images/designerai.png';
 const App = () => {
   const [showAboutMe, setShowAboutMe] = useState(true);
   const [showPortfolio, setShowPortfolio] = useState(false);
@@ -22,6 +23,13 @@ const App = () => {
 
   const projects = [
     {
+      title: 'Designer AI',
+      description: 'React based app for designing websites',
+      image: designerAI,
+      deployedLink: 'https://designer-ai-a4b729b9dfd7.herokuapp.com/',
+      githubLink: 'https://github.com/michaelayers3/project-3',
+    },
+    {
       title: 'Remind Me',
       description: 'Email based reminder app',
       image: remindImage,
@@ -29,18 +37,18 @@ const App = () => {
       githubLink: 'https://github.com/jmichaeldavis/remind-me',
     },
     {
-      title: 'JavaScript Quiz',
-      description: 'Simple quiz using JavaScript',
-      image: jsImage,
-      deployedLink: 'https://michaelayers3.github.io/Web-APIs-Code-Quiz/',
-      githubLink: 'https://github.com/michaelayers3/Web-APIs-Code-Quiz',
-    },
-    {
       title: 'Where the Flick',
       description: 'Streaming movie search using APIs',
       image: WTFimage,
       deployedLink: 'https://ashtreid.github.io/where-the-flick/',
       githubLink: 'https://github.com/ashtreid/where-the-flick',
+    },
+    {
+      title: 'JavaScript Quiz',
+      description: 'Simple quiz using JavaScript',
+      image: jsImage,
+      deployedLink: 'https://michaelayers3.github.io/Web-APIs-Code-Quiz/',
+      githubLink: 'https://github.com/michaelayers3/Web-APIs-Code-Quiz',
     },
     {
       title: 'Work Day Scheduler',
