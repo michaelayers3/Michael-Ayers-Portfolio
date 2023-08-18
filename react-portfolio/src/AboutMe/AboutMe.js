08150da704e9f20d9b9a0c8ba49242d48da588e9
michaelayers3 <michaelgregoryayers@gmail.com>
2023-08-18 16:30:12 -0500

    more icon stuff. finally got it.

--- react-portfolio/src/AboutMe/AboutMe.js
+++ react-portfolio/src/AboutMe/AboutMe.js
@@ -1,15 +1,18 @@
 
 import React from 'react';
 import meImg from '../images/megc.jpg';
-
+import headshot from '../images/headshot.jpeg';
 const AboutMe = () => {
   return (
     <div class = 'about'>
       <h2>About Me</h2>
       <div className = 'image-wrapper'>
-      <img src ={meImg} alt = 'itsme' />
+      <img src ={headshot} alt = 'itsme' />
       <div className ='details'>
-      <p>I love coding, basketball, skateboarding, and yoga.</p>
+      <p>I recently graduated from the </p>
+      <p> University of Utah's Fullstack Coding Bootcamp</p>
+       <p> and am eager to learn more about the world of coding!</p>
+       <p> I love coding, basketball, skateboarding, and yoga. </p>
       </div>
       </div>
       {/* Add other content for the "About Me" section */}
